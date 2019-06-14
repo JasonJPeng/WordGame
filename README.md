7428ce0388ede1efa0834531b3ea61f4468dff9a
Jason Peng <jason.j.peng@gamil.com>
2019-06-13 18:46:52 -0700

    sounds added, longer list, better design

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # WordGame
 UCLA Project W2-2 Word guessing Game
+This game is to test you knowledge about Crypto currencies
+
+
+Press "Start" to play the game
+You can guess up to 2 times of the crypto name's character number
+you don't see the answer if you lose the game. 
+
+Every time you lose or win, a new game stars and new crypto symbol displays
+and you start to guess again
--- myjavascript.js
+++ myjavascript.js
@@ -16,17 +16,44 @@ var listQ = [
     ["Cosmos", "https://s2.coinmarketcap.com/static/img/coins/32x32/3794.png"],
     ["Tezos", "https://s2.coinmarketcap.com/static/img/coins/32x32/2011.png"],
     ["IOTA", "https://s2.coinmarketcap.com/static/img/coins/32x32/1720.png"],
-    ["NEM", "https://s2.coinmarketcap.com/static/img/coins/32x32/873.png"]
+    ["NEM", "https://s2.coinmarketcap.com/static/img/coins/32x32/873.png"],
+    ["Ethereum Classic", "https://s2.coinmarketcap.com/static/img/coins/32x32/1321.png"],
+    ["Maker", "https://s2.coinmarketcap.com/static/img/coins/32x32/1518.png"],
+    ["Zcash", "https://s2.coinmarketcap.com/static/img/coins/32x32/1437.png"],
+    ["Chainlink", "https://s2.coinmarketcap.com/static/img/coins/32x32/1975.png"],
+    ["Bitcoin Gold", "https://s2.coinmarketcap.com/static/img/coins/32x32/2083.png"],
+    ["BAT", "https://s2.coinmarketcap.com/static/img/coins/32x32/1697.png"],
+    ["Dogecoin", "https://s2.coinmarketcap.com/static/img/coins/32x32/74.png"],
+    ["Qtum", "https://s2.coinmarketcap.com/static/img/coins/32x32/1684.png"],
+    ["OmiseGO", "https://s2.coinmarketcap.com/static/img/coins/32x32/1808.png"],
+    [" Zilliqa", "https://s2.coinmarketcap.com/static/img/coins/32x32/2469.png"],
+    ["Augur", "https://s2.coinmarketcap.com/static/img/coins/32x32/1104.png"],
+    ["0x", "https://s2.coinmarketcap.com/static/img/coins/32x32/1896.png"],
+    ["Steem", "https://s2.coinmarketcap.com/static/img/coins/32x32/1230.png"],
+    ["Siacoin", "https://s2.coinmarketcap.com/static/img/coins/32x32/1042.png"],
+    ["WAX", "https://s2.coinmarketcap.com/static/img/coins/32x32/2300.png"],
+    ["Zcoin", "https://s2.coinmarketcap.com/static/img/coins/32x32/1414.png"],
+    ["Litecoin", "https://s2.coinmarketcap.com/static/img/coins/32x32/2.png"]
    ]
 
 var str;
 var ans = [], guessing = [], guessed = []  
 var maxGuess
 var wins=0, losses=0;
+// var snd = new Audio("media/beep.mp3"); // buffers automatically when created
+// snd.play();
+
+var winSnd = new Audio("media/Rooster.mp3");
+var lossSnd = new Audio("media/Police.mp3");
+var audio = new Audio("media/beep-short.mp3");
+// audio.src = ;
+// audio.play();
 
 function startNewGame() {
 
    var nQ = Math.floor(Math.random() * listQ.length);
+
+//    audio.play();
    
    str = listQ[nQ][0];
    var str1 = str.replace(/\s/g, "").toUpperCase(); // no sapce Upper
@@ -62,16 +89,24 @@ document.onkeyup = function (e) {
 
     if (maxGuess >= 0 ) {// Keep guessing
         if (guessing.indexOf("_") < 0) { // Got it, No "_" in the array
-            alert ("You won. The coin name is :" + str);
+            winSnd.play();
             document.getElementById("wins").innerHTML = ++wins;
+            document.getElementById("guessing").innerHTML = guessing.join(" ");
+            alert ("You won. The coin name is :" + str);
+            
             startNewGame();            
         }        
     }   else {  // lost the game
-        alert ("You don't guess right !");
+        lossSnd.play();
         document.getElementById("losses").innerHTML = ++losses;
+        document.getElementById("guessed").innerHTML = guessed.join(",");
+        alert ("You don't guess right !");
+        
         startNewGame();
     }
 
+  } else {
+      audio.play();
   }
 
 }
